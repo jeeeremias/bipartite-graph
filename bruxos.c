b2195588d0bef1e86e68d7a2c18877852f934882
Jeremias Reis <jeeeremias@users.noreply.github.com>
2018-10-06 14:43:54 -0300

    Added source file and problem description

---
+++ bruxos.c
@@ -0,0 +1,151 @@
+/*
+ ============================================================================
+ Author      : Jeremias Pereira dos Reis
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VERTEX 2000
+#define RED 1
+#define BLUE 0
+#define TRUE 1
+#define FALSE 0
+
+typedef struct Graph_Vertex
+{
+  int value;
+  char visited;
+  char color;
+  struct Vertex_Adjacency* adjacent;
+} Vertex;
+
+typedef struct Vertex_Adjacency
+{
+  struct Graph_Vertex* vertex;
+  struct Vertex_Adjacency* next;
+} VertexAdjacent;
+
+Vertex graph[MAX_VERTEX];
+Vertex* queue[MAX_VERTEX];
+
+void
+init_graph ()
+{
+  for (int i = 0; i < MAX_VERTEX; i++)
+    {
+      graph[i].value = i + 1;
+      graph[i].visited = FALSE;
+      graph[i].adjacent = NULL;
+    }
+}
+
+void
+clean_graph (int n)
+{
+  VertexAdjacent* adjacent;
+  VertexAdjacent* adjacent_aux;
+  for (int i = 0; i < MAX_VERTEX; i++)
+    {
+      graph[i].visited = FALSE;
+      adjacent = graph[i].adjacent;
+      while (adjacent != NULL)
+	{
+	  adjacent_aux = adjacent->next;
+	  free (adjacent);
+	  adjacent = adjacent_aux;
+	}
+      graph[i].adjacent = NULL;
+    }
+}
+
+void
+addEdge (int u, int v)
+{
+  VertexAdjacent* adjacent;
+  VertexAdjacent* adjacent_aux;
+  if (graph[u - 1].adjacent == NULL)
+    {
+      graph[u - 1].adjacent =
+	  (VertexAdjacent*) (malloc (sizeof(VertexAdjacent)));
+      graph[u - 1].adjacent->vertex = &graph[v - 1];
+      graph[u - 1].adjacent->next = NULL;
+    }
+  else
+    {
+      // Sorted
+      adjacent = graph[u - 1].adjacent;
+      while (adjacent->next != NULL && adjacent->vertex->value < v)
+	{
+	  adjacent = adjacent->next;
+	}
+      if (adjacent->next == NULL || adjacent->next->vertex->value != v)
+	{
+	  adjacent_aux = adjacent->next;
+	  adjacent->next = (VertexAdjacent*) (malloc (sizeof(VertexAdjacent)));
+	  adjacent->next->vertex = &graph[v - 1];
+	  adjacent->next->next = adjacent_aux;
+	}
+    }
+}
+
+int
+main (void)
+{
+  int N, M, u, v, c, i, j, init_queue, end_queue;
+  char bipartite;
+  VertexAdjacent* adjacent;
+  init_graph ();
+  while (scanf ("%d %d", &N, &M) && (N != 0 || M != 0))
+    {
+      bipartite = TRUE;
+      init_queue = 0;
+      end_queue = 0;
+      for (c = 0; c < M; c++)
+	{
+	  scanf ("%d %d", &u, &v);
+	  addEdge (u, v);
+	  addEdge (v, u);
+	}
+      queue[end_queue++] = &graph[init_queue];
+      graph[init_queue].color = RED;
+      graph[init_queue].visited = TRUE;
+      while (bipartite && init_queue != end_queue)
+	{
+	  adjacent = queue[init_queue]->adjacent;
+	  while (bipartite && adjacent != NULL)
+	    {
+	      if (!adjacent->vertex->visited)
+		{
+		  if (queue[init_queue]->color == RED)
+		    {
+		      adjacent->vertex->color = BLUE;
+		    }
+		  else
+		    {
+		      adjacent->vertex->color = RED;
+		    }
+		  adjacent->vertex->visited = TRUE;
+		  queue[end_queue++] = adjacent->vertex;
+		}
+	      if (adjacent->vertex->color == queue[init_queue]->color) {
+		  bipartite = FALSE;
+		  break;
+	      }
+	      adjacent = adjacent->next;
+	    }
+	  init_queue ++;
+	}
+      if (bipartite)
+	{
+	  printf("SIM\n");
+	}
+      else
+	{
+	  printf("NAO\n");
+	}
+
+      clean_graph (N);
+    }
+}
